a51dafbb81275a7b398b44de43a2f8e29fc87503
alanff97 <alan.figueroa.cl@gmail.com>
2024-01-29 16:40:42 +0100

    Item Component

---
+++ src/components/Item.tsx
@@ -0,0 +1,14 @@
+export function Item({
+  text,
+  handleClick,
+}: {
+  text: string;
+  handleClick: () => void;
+}) {
+  return (
+    <li>
+      {text}
+      <button onClick={handleClick}>Eliminar Elemento</button>
+    </li>
+  );
+}
